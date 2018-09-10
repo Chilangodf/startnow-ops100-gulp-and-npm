6e69f9fed956dd0c57c3801f748f6e63aec25c9c
chilangodf <jcmm1255@gmail.com>
2018-09-10 12:54:31 -0700

    Initial push

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+// write your code here
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const nodemon = require('gulp-nodemon');
+
+module.exports = gulp;
